114de72f161ab4c7cb5f479420e034e086c7a1fe
MogadasuHarini <134291874+MogadasuHarini@users.noreply.github.com>
2025-09-14 08:15:29 +0530

    Update Assement.js

--- Assement.js
+++ Assement.js
@@ -1,5 +1,4 @@
 
-```jsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -504,5 +503,6 @@ render();
 </script>
 </body>
 </html>
+
 
 
